1630e3df3049b5cc4b161e17d278c3636ddfcfe8
Jose Thomas <jostho@gmail.com>
2020-08-07 22:27:40 +0530

    Simplify integration test
    .. and remove dev dependency on reqwest
    
    Use actions checkout to prepare for CI.

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,26 +1,9 @@
-use reqwest::blocking;
 use std::error::Error;
-use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 
-const COUNTRIES_JSON_REMOTE: &str =
-    "https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.json";
-
-fn prepare_countries_json(input_path: &str, output_path: &str) -> Result<(), Box<dyn Error>> {
-    // clean up first
-    if Path::new(output_path).is_file() {
-        fs::remove_file(output_path)?;
-    }
-    // get from the remote location
-    if !Path::new(input_path).is_file() {
-        println!("Getting countries.json from remote");
-        let body = blocking::get(COUNTRIES_JSON_REMOTE)?.text()?;
-        let _result = fs::write(input_path, body);
-    }
-    Ok(())
-}
+const COUNTRIES_JSON: &str = "target/countries/dist/countries.json";
 
 fn get_line_count(path: &str) -> Result<usize, Box<dyn Error>> {
     let file = File::open(Path::new(path))?;
@@ -31,16 +14,11 @@ fn get_line_count(path: &str) -> Result<usize, Box<dyn Error>> {
 #[test]
 #[ignore]
 fn generate_content_for_remote_countries_json() {
-    let countries_json = "target/countries.json";
-    let countries_csv = "target/countries.csv";
-
-    let _result = prepare_countries_json(&countries_json, &countries_csv);
-    quest::generate_content(&countries_json, &countries_csv);
-
-    assert!(Path::new(&countries_json).is_file());
+    let countries_csv = "target/countries-1.csv";
+    quest::generate_content(&COUNTRIES_JSON, &countries_csv);
     assert!(Path::new(&countries_csv).is_file());
 
-    let lc_json = get_line_count(&countries_json).unwrap();
+    let lc_json = get_line_count(&COUNTRIES_JSON).unwrap();
     let lc_csv = get_line_count(&countries_csv).unwrap();
     // csv file has extra line - for header
     assert_eq!(lc_json, lc_csv - 1);
